28dd776a45cae41935a77f8f3fce61cf53efa220
Mugisha-Joshua <o.mugisha2@alustudent.com>
2026-09-23 14:43:53 +0200

    Add Header component with logo and New Post link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from './components/Header';
+
 function App() {
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="logo">Dev Insights</h1>
+      <nav>
+        <a href="#">New Post</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
